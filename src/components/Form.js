ebc6e3d9a0ec8b7939ac821ea17494a5cf45dc43
Nícolas <nicolasbrga01@gmail.com>
2022-12-01 16:08:34 -0300

    requisito 2 [meia hora por causa de UMA palavra (basicamente)]

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo,
+      isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
     return (
       <form>
         <input
           type="text"
           data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
         />
         <input
           type="textArea"
           data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
         />
         <input
           type="text"
           data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
         />
-        <select data-testid="rare-input">
+        <select data-testid="rare-input" onChange={ onInputChange } value={ cardRare }>
           <option value="normal">normal</option>
           <option value="raro">raro</option>
           <option value="muito raro">muito raro</option>
@@ -36,10 +52,14 @@ class Form extends Component {
         <input
           type="checkbox"
           data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
         />
         <button
           data-testid="save-button"
           type="submit"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
         >
           Salvar
         </button>
@@ -47,5 +67,20 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  hasTrunfo: PropTypes.bool,
+  isSaveButtonDisabled: PropTypes.bool,
+  onInputChange: PropTypes.func,
+  onSaveButtonClick: PropTypes.func,
+}.isRequired;
 
 export default Form;
