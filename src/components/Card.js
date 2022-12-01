7d3ad3f38fe4a056c44375c32d04896faf31dccd
Nícolas <nicolasbrga01@gmail.com>
2022-12-01 17:06:40 -0300

    advinha quem é? é o lint

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends Component {
-  render () {
+  render() {
     const { cardName, cardDescription, cardAttr1,
       cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo } = this.props;
     return (
       <>
-      <h1 data-testid="name-card">{ cardName }</h1>
-      <img data-testid="image-card" src={ cardImage } alt={ cardName }></img>
-      <h2 data-testid="description-card">{ cardDescription }</h2>
-      <p data-testid="attr1-card">{ cardAttr1 }</p>
-      <p data-testid="attr2-card">{ cardAttr2 }</p>
-      <p data-testid="attr3-card">{ cardAttr3 }</p>
-      <p data-testid="rare-card">{ cardRare }</p>
-      {cardTrunfo && 
-        <p data-testid="trunfo-card"> Super Trunfo </p>
-      }
+        <h1 data-testid="name-card">{ cardName }</h1>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <h2 data-testid="description-card">{ cardDescription }</h2>
+        <p data-testid="attr1-card">{ cardAttr1 }</p>
+        <p data-testid="attr2-card">{ cardAttr2 }</p>
+        <p data-testid="attr3-card">{ cardAttr3 }</p>
+        <p data-testid="rare-card">{ cardRare }</p>
+        {cardTrunfo
+        && <p data-testid="trunfo-card"> Super Trunfo </p>}
       </>
-    )
+    );
   }
 }
 
