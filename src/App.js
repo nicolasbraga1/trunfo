040f0f866dda1036448efd868d47eefc68f8a5ae
Nícolas <nicolasbrga01@gmail.com>
2022-12-02 16:49:42 -0300

    sempre o lint

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ class App extends React.Component {
     cardImage: '',
     cardRare: '',
     isSaveButtonDisabled: true,
-  }
+  };
+
   validateBtn = () => {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
-      cardAttr3, cardImage, cardRare,} = this.state
+      cardAttr3, cardImage, cardRare } = this.state;
     const cost = 90;
     const maxCost = 210;
     const checkName = cardName.length !== 0;
@@ -23,14 +24,14 @@ class App extends React.Component {
     const checkAttr1 = Number(cardAttr1) >= 0 && Number(cardAttr1) <= cost;
     const checkAttr2 = Number(cardAttr2) >= 0 && Number(cardAttr2) <= cost;
     const checkAttr3 = Number(cardAttr3) >= 0 && Number(cardAttr3) <= cost;
-    const checkTotal = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) 
+    const checkTotal = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)
     <= maxCost;
     const checkImage = cardImage.length !== 0;
     const checkRare = cardRare.length !== 0;
     this.setState({
       isSaveButtonDisabled: !(
-        checkName && checkDescription && checkAttr1 && checkAttr2 && checkAttr3 && 
-        checkTotal && checkImage && checkRare
+        checkName && checkDescription && checkAttr1 && checkAttr2 && checkAttr3
+        && checkTotal && checkImage && checkRare
       ),
     });
   };
